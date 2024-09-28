a874aa62e4028c9c7c4b0152076fa8a777414399
Vladimir <sv_urich@mail.ru>
2024-09-28 14:22:36 +0400

    Add files via upload

---
+++ module_9_6.py/main.py
@@ -0,0 +1,19 @@
+from itertools import combinations
+
+
+def all_variants(text):
+    lis = []
+    for i in range(1, len(text) + 1):
+        lis = []
+        b = combinations(text, i)
+        lis.extend(b)
+        for j in lis:
+            j = list(j)
+            yield (''.join(j))
+
+
+a = all_variants("abc")
+for i in a:
+    if i == 'ac':
+        continue
+    print(i)
